d2e6f584973f7107f66eab96e9b98d8bf8a99ff6
sm-rijal <srijal@binaracademy.org>
2024-04-03 23:33:45 +0700

    add ch9day5, integrasi crud and add chart

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -10,11 +10,17 @@ function Product() {
     const [isLoading, setIsLoading] = useState(false);
 
     const getData = async() => {
-        setIsLoading(true)
-        const response = await axios.get('http://localhost:8000/products')
-        console.log(response.data);
-        setData(response.data)
-        setIsLoading(false)
+        try {
+            setIsLoading(true)
+            const response = await axios.get('http://localhost:8000/products')
+            console.log(response.data);
+            setData(response.data)
+            setIsLoading(false)
+            
+        } catch (error) {
+            console.log(error);
+            setIsLoading(false)
+        }
     }
 
     useEffect(() => {
